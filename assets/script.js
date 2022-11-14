5eecd4cb98c620f5ca8995d1f04bf862e4850d86
Reeth Dasgupta <115037176+reethd@users.noreply.github.com>
2022-11-14 15:38:25 -0500

    created array of questions

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,103 @@
-var timeEl = document.querySelector(".timer");
-var secondsLeft = 100;
+let timeEl = document.querySelector(".timer");
+let secondsLeft = 100;
+let startButton = document.querySelector("#start");
+let quizQuestions = [
+  {
+    question: "Question 1",
+    answers: {
+      a: "a",
+      b: "b",
+      c: "c",
+    },
+    correctAnswer: "a",
+  },
+  {
+    question: "Question 2",
+    answers: {
+      a: "a",
+      b: "b",
+      c: "c",
+    },
+    correctAnswer: "b",
+  },
+  {
+    question: "Question 3",
+    answers: {
+      a: "a",
+      b: "b",
+      c: "c",
+    },
+    correctAnswer: "c",
+  },
+  {
+    question: "Question 4",
+    answers: {
+      a: "a",
+      b: "b",
+      c: "c",
+    },
+    correctAnswer: "c",
+  },
+  {
+    question: "Question 5",
+    answers: {
+      a: "a",
+      b: "b",
+      c: "c",
+    },
+    correctAnswer: "a",
+  },
+  {
+    question: "Question 6",
+    answers: {
+      a: "a",
+      b: "b",
+      c: "c",
+    },
+    correctAnswer: "b",
+  },
+  {
+    question: "Question 7",
+    answers: {
+      a: "a",
+      b: "b",
+      c: "c",
+    },
+    correctAnswer: "a",
+  },
+  {
+    question: "Question 8",
+    answers: {
+      a: "a",
+      b: "b",
+      c: "c",
+    },
+    correctAnswer: "c",
+  },
+  {
+    question: "Question 9",
+    answers: {
+      a: "a",
+      b: "b",
+      c: "c",
+    },
+    correctAnswer: "b",
+  },
+  {
+    question: "Question 10",
+    answers: {
+      a: "a",
+      b: "b",
+      c: "c",
+    },
+    correctAnswer: "c",
+  }
+];
 
-document.querySelector("#start").addEventListener("click", startTimer);
+startButton.addEventListener("click", startTimer);
+startButton.addEventListener("click", startQuiz);
+
+function startQuiz() {}
 
 function startTimer() {
   var timerInterval = setInterval(function () {
@@ -16,8 +112,4 @@ function startTimer() {
   }, 1000);
 }
 
-function quizOver() {
-    
-}
-
-
+function quizOver() {}
